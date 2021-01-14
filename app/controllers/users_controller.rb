4dc57eac256a60cf122196b53f87174c78083856
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-01-14 10:25:34 +0900

    Active Storageの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :name, :profile)
+    params.require(:user).permit(:email, :name, :profile, :image)
   end
 
   def set_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_one_attached :image
+
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
 end
