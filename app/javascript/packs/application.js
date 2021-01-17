5043b2beb146da6ad0a5ee96a8a0e036deea9809
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-01-17 16:44:38 +0900

    スクロールボタン追加
    それに伴い、jQuery導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('./slide_menu')
+require('./scroll')
+require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
