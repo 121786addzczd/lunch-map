f101529a0409acc79fc202f177fa364d0efca412
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2021-01-17 22:17:16 +0900

    カード作成機能実装

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
   belongs_to :user
 
+  has_many :cards, dependent: :destroy
+
   # 文字数が1文字以上255文字以下であるかどうかを検証するバリデーション
   validates :title, length: { in: 1..255 }
 end
